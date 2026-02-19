2050f960fd95081442803c32892142eacdb1587f
Jean Zoundi <jeanzoundi3@gmail.com>
2026-02-19 15:19:49 +0000

    fix ordre supabase

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1200, // augmente la limite (en kB)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+          if (id.includes('recharts')) {
+            return 'charts'
+          }
+        }
+      }
+    }
+  }
 })
